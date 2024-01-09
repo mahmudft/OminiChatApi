f45ba014380aa1e21993eca500b695c80f773321
NihadAxund <coinnihad@gmail.com>
2024-01-10 02:56:00 +0400

    #

--- entities/chat.entity.js
+++ entities/chat.entity.js
@@ -6,11 +6,11 @@ const mongoose = await createConnection()
 const ChatSchema = new mongoose.Schema({
     receiverId: {
       type: String,
-      required: [true, 'Receiver ID is required'],
+     
     },
     senderId: {
       type: String,
-      required: [true, 'Sender ID is required'],
+     
     },
     owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     messages: [{
--- routers/chatRouter.js
+++ routers/chatRouter.js
@@ -1,16 +1,15 @@
-import express from "express"
-import { User } from "../entities/user.entity.js"
+import express from "express";
+import { User } from "../entities/user.entity.js";
 import { Message } from "../entities/message.entity.js";
 import { Chat } from "../entities/chat.entity.js";
-export const chat = express.Router()
 
-// mytoken eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1OWQ0YWQ1YjFiMzExZjllMWE4OGM5YyIsImVtYWlsIjoibmloYXRheHVuZHphZGUwNkBnbWFpbC5jb20iLCJpYXQiOjE3MDQ4MDcxMzgsImV4cCI6MTcwNDk3OTkzOH0.-HfU0VBg_2Qb_CExh6YFhGppuns5tnnGWQxSGQbYKbNtjDsdzWf1DyemJpNXA9Hc30j61SpDxGZQXsPJ4CfxTg
-
+export const chat = express.Router();
+////////////////////// DUZEDIM
 chat.post("/sendmessage", async (req, res) => {
-    // Body{obj.receiverId,obj.content}
-    console.log("----------------------------------------------")
+    // Body{obj.receiverId, obj.content}
+    console.log("----------------------------------------------");
     try {
-        let obj = req.body;
+        const obj = req.body;
         const currentUser = await User.findById(req.user.id).populate({
             path: 'chatList',
             match: {
@@ -19,8 +18,8 @@ chat.post("/sendmessage", async (req, res) => {
             }
         });
 
-        console.log("\n")
-        console.log(obj.receiverId)
+        console.log("\n");
+        console.log(obj.receiverId);
         const receiverUser = await User.findById(obj.receiverId).populate({
             path: 'chatList',
             match: {
@@ -28,86 +27,88 @@ chat.post("/sendmessage", async (req, res) => {
                 senderId: req.user.id
             }
         });
-        console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+
+        console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 
         if (!currentUser) {
-            console.log("Current User empty")
+            console.log("Current User empty");
+            return;
+        } else if (!receiverUser) {
+            console.log("Receiver User empty");
             return;
         }
-        else if (!receiverUser) {
-            console.log("ReceiverUser User empty")
-            return;
-        }
 
-        console.log("currrent user chat list \n")
-        console.log(currentUser.chatList.length)
+        console.log("current user chat list \n");
+        console.log(currentUser.chatList.length);
 
-        if (!currentUser.chatList || currentUser.chatList.length == 0) {
-            const chatldata = new Chat({
-                receiverId: obj.receiverId,
-                senderId: currentUser._id,
-                owner: currentUser._id
+        // xetta bunun cagridgi ifinin icersinde idi hele eldim
+        const createAndSaveChat = async (user, senderId, receiverId) => {
+            const chatldata = await Chat.create({
+                receiverId: receiverId,
+                senderId: senderId,
+                owner: user.id,
+                messages: []
             });
-            chatldata.messages = [];
-            currentUser.chatList.push(chatldata._id);
-            await chatldata.save();
-            await currentUser.save();
 
+            user.chatList.push(chatldata._id);
+
+            await user.save();
+            return chatldata;
+        };
+
+        if (!currentUser.chatList || currentUser.chatList.length === 0) {
+
+            const chatListData = await createAndSaveChat(currentUser, req.user.id, obj.receiverId);
+            currentUser.chatList = [chatListData]; /// xetabunda imis yazdim duzeldi bele tezden beraber edende
         }
 
-        console.log(receiverUser.chatList.length)
-        console.log("\nifffffffffffff\n")
+        console.log(receiverUser.chatList.length);
+        console.log("\nifffffffffffff\n");
 
-        if (!receiverUser.chatList || receiverUser.chatList.length == 0) {
-            const chatldata = new Chat({
-                receiverId: obj.receiverId,
-                senderId: currentUser._id,
-                owner: receiverUser._id
-            });
-            chatldata.messages = [];
-            receiverUser.chatList.push(chatldata._id);
-            await chatldata.save();
-            await receiverUser.save();
-
+        if (!receiverUser.chatList || receiverUser.chatList.length === 0) {
+            const chatListData = await createAndSaveChat(receiverUser, req.user.id, obj.receiverId);
+            receiverUser.chatList = [chatListData]; /// xeta bunda imis
         }
-        console.log("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzSECTION 2zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz")
-        console.log(currentUser.chatList[0]);
-        const message = new Message({
+
+        console.log("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzSECTION 2zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+        console.log(receiverUser);
+
+        // Now, after ensuring that chat lists are created and saved, proceed to message creation
+        const chatIdCurrentUser = currentUser.chatList[0].id;
+        const chatIdReceiverUser = receiverUser.chatList[0].id;
+
+        const message = await Message.create({
             content: obj.content,
             receiverId: obj.receiverId,
-            senderId: currentUser._id,
-            chat: currentUser.chatList[0].id
+            senderId: req.user.id,
+            chat: chatIdCurrentUser
         });
 
-
-        const message2 = new Message({
+        const message2 = await Message.create({
             content: obj.content,
             receiverId: obj.receiverId,
-            senderId: currentUser._id,
-            chat: receiverUser.chatList[0].id
+            senderId: req.user.id,
+            chat: chatIdReceiverUser
         });
-        console.log("\n ")
+
+        console.log("\n ");
         console.log(receiverUser.chatList[0]);
+        // savelerde awaitleri sildim ondan sonra problem hell oldu 
         currentUser.chatList[0].messages.push(message._id);
         receiverUser.chatList[0].messages.push(message2._id);
-        await message.save();
-        await message2.save();
-        await currentUser.chatList[0].save();
-        await receiverUser.chatList[0].save();
-        console.log(receiverUser.chatList[0].messages)
-        await currentUser.save();
-        await receiverUser.save();
-
-
+        // await Promise.all([
+        // ]);
+        await currentUser.chatList[0].save(),
+        await receiverUser.chatList[0].save(),
+        await currentUser.save(),
+        await receiverUser.save()
+        
+        console.log(receiverUser.chatList[0].messages);
         console.log("--------- send message section ------");
-        // console.log(obj);
-        // console.log(currentUser)
-        // console.log(receiverUser);
-        return"ture"
+
+        return "true";
     } catch (error) {
-        console.log(error)
-        return error
+        console.log(error);
+        return error;
     }
-
-
-})
+});
